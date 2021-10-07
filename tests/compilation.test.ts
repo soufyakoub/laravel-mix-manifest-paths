5b736ce0e26856f8dbcb90ecf5d69d3d4ee8e493
soufyakoub <splentercell.1997@gmail.com>
2021-10-07 21:33:22 +0100

    test: test the escape delimiters

--- tests/compilation.test.ts
+++ tests/compilation.test.ts
@@ -21,9 +21,9 @@ beforeEach(() => {
 			+ "\n{{mix('/txt/sub/b.txt')}}",
 			"b.txt": "blabla {{mix('/js/app.js')}}"
 			+ "\nmore blabla {{mix('/css/app.css')}}"
-			+ "\n{{mix('/txt/c.txt')}}",
+			+ "\n{{! mix('/txt/<c>.txt') !}}",
 			"sub": {
-				"c.txt": "ufhqfjh {{mix('/README.md')}}"
+				"<c>.txt": "ufhqfjh {{mix('/README.md')}}"
 				+ "\nergtseyerqei zeot h zei",
 				"d.txt": "ufhqfjh {{mix('/README.md')}}"
 				+ "\nergtseyerqei zeot h zei"
@@ -75,9 +75,9 @@ const entries: ResolvedEntry[] = [
 		options: {...defaultOptions},
 	},
 	{
-		src: path.resolve("resources/sub/c.txt"),
-		dest: path.resolve("public/txt/c.txt"),
-		public_url: "/txt/c.txt",
+		src: path.resolve("resources/sub/<c>.txt"),
+		dest: path.resolve("public/txt/<c>.txt"),
+		public_url: "/txt/<c>.txt",
 		options: {...defaultOptions},
 	},
 	{
@@ -96,14 +96,14 @@ test("build", () => {
 	const a = fs.readFileSync("public/txt/a.txt", "utf-8");
 	const b = fs.readFileSync("public/txt/b.txt", "utf-8");
 	const sub_b = fs.readFileSync("public/txt/sub/b.txt", "utf-8");
-	const c = fs.readFileSync("public/txt/c.txt", "utf-8");
+	const c = fs.readFileSync("public/txt/<c>.txt", "utf-8");
 	const d = fs.readFileSync("public/txt/d.txt", "utf-8");
 
 	const expected_c = "ufhqfjh /README.md?id=bf15a78c28f46e55abc5"
 		+ "\nergtseyerqei zeot h zei";
 	const expected_b = "blabla /js/app.js?id=752e64981810d0203520"
 		+ "\nmore blabla /css/app.css?id=68b329da9893e34099c7"
-		+ `\n/txt/c.txt?id=${md5(expected_c, 20)}`;
+		+ `\n/txt/&lt;c&gt;.txt?id=${md5(expected_c, 20)}`;
 	const expected_sub_b = expected_b;
 	const expected_a = `random txt /txt/b.txt?id=${md5(expected_b, 20)} qsdh`
 	+ `\n/txt/sub/b.txt?id=${md5(expected_sub_b, 20)}`;
@@ -117,7 +117,7 @@ test("build", () => {
 		"/txt/a.txt": `/txt/a.txt?id=${md5(expected_a, 20)}`,
 		"/txt/b.txt": `/txt/b.txt?id=${md5(expected_b, 20)}`,
 		"/txt/sub/b.txt": `/txt/sub/b.txt?id=${md5(expected_sub_b, 20)}`,
-		"/txt/c.txt": `/txt/c.txt?id=${md5(expected_c, 20)}`,
+		"/txt/<c>.txt": `/txt/<c>.txt?id=${md5(expected_c, 20)}`,
 		"/txt/d.txt": `/txt/d.txt?id=${md5(expected_d, 20)}`,
 	};
 
@@ -139,9 +139,9 @@ test("build", () => {
 	// d
 	expect(directDependenciesOf("/txt/d.txt")).toEqual(["/txt/a.txt"]);
 	expect(directDependenciesOf("/txt/a.txt").sort()).toEqual(["/txt/b.txt", "/txt/sub/b.txt"]);
-	expect(directDependenciesOf("/txt/b.txt")).toEqual(["/txt/c.txt"]);
-	expect(directDependenciesOf("/txt/sub/b.txt")).toEqual(["/txt/c.txt"]);
-	expect(directDependenciesOf("/txt/c.txt")).toEqual([]);
+	expect(directDependenciesOf("/txt/b.txt")).toEqual(["/txt/<c>.txt"]);
+	expect(directDependenciesOf("/txt/sub/b.txt")).toEqual(["/txt/<c>.txt"]);
+	expect(directDependenciesOf("/txt/<c>.txt")).toEqual([]);
 	expect(depGraph.size()).toBe(5);
 });
 
@@ -150,7 +150,7 @@ test("watch", () => {
 		+ "\nergtseyerqei zeot h zei";
 	const build_b = "blabla /js/app.js?id=752e64981810d0203520"
 		+ "\nmore blabla /css/app.css?id=68b329da9893e34099c7"
-		+ `\n/txt/c.txt?id=${md5(build_c, 20)}`;
+		+ `\n/txt/&lt;c&gt;.txt?id=${md5(build_c, 20)}`;
 	const build_sub_b = build_b;
 	const build_a = `random txt /txt/b.txt?id=${md5(build_b, 20)} qsdh`
 	+ `\n/txt/sub/b.txt?id=${md5(build_sub_b, 20)}`;
@@ -164,7 +164,7 @@ test("watch", () => {
 		"/txt/a.txt": `/txt/a.txt?id=${md5(build_a, 20)}`,
 		"/txt/b.txt": `/txt/b.txt?id=${md5(build_b, 20)}`,
 		"/txt/sub/b.txt": `/txt/sub/b.txt?id=${md5(build_b, 20)}`,
-		"/txt/c.txt": `/txt/c.txt?id=${md5(build_c, 20)}`,
+		"/txt/<c>.txt": `/txt/<c>.txt?id=${md5(build_c, 20)}`,
 		"/txt/d.txt": `/txt/d.txt?id=${md5(build_d, 20)}`,
 	};
 
@@ -184,14 +184,14 @@ test("watch", () => {
 	depGraph.addDependency("/txt/d.txt", "/txt/a.txt");
 	depGraph.addDependency("/txt/a.txt", "/txt/b.txt");
 	depGraph.addDependency("/txt/a.txt", "/txt/sub/b.txt");
-	depGraph.addDependency("/txt/b.txt", "/txt/c.txt");
-	depGraph.addDependency("/txt/sub/b.txt", "/txt/c.txt");
+	depGraph.addDependency("/txt/b.txt", "/txt/<c>.txt");
+	depGraph.addDependency("/txt/sub/b.txt", "/txt/<c>.txt");
 
 	fs.mkdirSync("public/txt/sub", {recursive: true});
 	fs.writeFileSync("public/txt/a.txt", build_b);
 	fs.writeFileSync("public/txt/b.txt", build_a);
 	fs.writeFileSync("public/txt/sub/b.txt", build_sub_b);
-	fs.writeFileSync("public/txt/c.txt", build_c);
+	fs.writeFileSync("public/txt/<c>.txt", build_c);
 	fs.writeFileSync("public/txt/d.txt", build_d);
 	fs.writeFileSync("public/mix-manifest.json", JSON.stringify(buildManifest));
 
@@ -211,7 +211,7 @@ test("watch", () => {
 	const a = fs.readFileSync("public/txt/a.txt", "utf-8");
 	const b = fs.readFileSync("public/txt/b.txt", "utf-8");
 	const sub_b = fs.readFileSync("public/txt/sub/b.txt", "utf-8");
-	const c = fs.readFileSync("public/txt/c.txt", "utf-8");
+	const c = fs.readFileSync("public/txt/<c>.txt", "utf-8");
 	const d = fs.readFileSync("public/txt/d.txt", "utf-8");
 
 	const expected_c = "ufhqfjh /README.md?id=bf15a78c28f46e55abc5"
@@ -231,7 +231,7 @@ test("watch", () => {
 		"/txt/a.txt": `/txt/a.txt?id=${md5(expected_a, 20)}`,
 		"/txt/b.txt": `/txt/b.txt?id=${md5(expected_b, 20)}`,
 		"/txt/sub/b.txt": `/txt/sub/b.txt?id=${md5(expected_sub_b, 20)}`,
-		"/txt/c.txt": `/txt/c.txt?id=${md5(expected_c, 20)}`,
+		"/txt/<c>.txt": `/txt/<c>.txt?id=${md5(expected_c, 20)}`,
 		"/txt/d.txt": `/txt/d.txt?id=${md5(expected_d, 20)}`,
 	};
 
@@ -255,7 +255,7 @@ test("watch", () => {
 	expect(directDependenciesOf("/txt/a.txt").sort()).toEqual(["/txt/b.txt", "/txt/sub/b.txt"]);
 	expect(directDependenciesOf("/txt/b.txt")).toEqual([]);
 	expect(directDependenciesOf("/txt/sub/b.txt")).toEqual([]);
-	expect(directDependenciesOf("/txt/c.txt")).toEqual([]);
+	expect(directDependenciesOf("/txt/<c>.txt")).toEqual([]);
 	expect(depGraph.size()).toBe(5);
 
 	const calls = onEntryCompiled.mock.calls;
